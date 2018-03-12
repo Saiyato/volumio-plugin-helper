803016ecf096e396d34c7501b9cfb4931f51b096
Rachid Groeneveld <Saiyato@users.noreply.github.com>
2018-03-12 14:55:57 +0100

    Minor patch

--- volumio_install_zip_from_fork.sh
+++ volumio_install_zip_from_fork.sh
@@ -1,7 +1,8 @@
 # Volumio installer (workaround for plugins/fixes not in the Volumio repo) - download from forked repo
 # $1 = author; $2 = category, $3 = plugin name, $4 = zip filename (optional)
-${4:=$3}
+ZIPFILE = ${4:=$3}
 echo "Downloading $2/$3"
+echo "Zip filename  = $ZIPFILE"
 if [ ! -d /home/volumio/$3 ];
 then
 	mkdir /home/volumio/$3
@@ -13,9 +14,9 @@ fi
 echo "Downloading and extracting zip file..."
 cd /home/volumio/$3
 
-wget -O $4.zip https://github.com/$1/volumio-plugins/raw/master/plugins/$2/$3/$4.zip
-miniunzip -xo $4.zip
-rm $4.zip
+wget -O $4.zip https://github.com/$1/volumio-plugins/raw/master/plugins/$2/$3/$ZIPFILE.zip
+miniunzip -xo $ZIPFILE.zip
+rm $ZIPFILE.zip
 
 echo "Installing plugin..."
 volumio plugin install
